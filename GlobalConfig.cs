ebd5f37e6f7b2ee0f388338c7c66f4bae2cc59ba
Krisgster <35160463+Krisgster@users.noreply.github.com>
2018-01-06 14:25:01 +0700

    Add files via upload

---
+++ GlobalConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Patterns.Singleton
+{
+    /// <summary>
+    /// http://www.oodesign.com/singleton-pattern.html
+    /// </summary>
+    public class GlobalConfig
+    {
+        public int MaxNumberOfPhotos { get; private set; }
+        public string PhotoDomainName { get; private set; }
+        public string CreatedTime { get; private set; }
+
+        private static GlobalConfig _configs;
+        private GlobalConfig()
+        {
+            //Load config from database
+            MaxNumberOfPhotos = 1985;
+
+            //Load config from database
+            PhotoDomainName = "Kris";
+
+            CreatedTime = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss");
+        }
+        public static GlobalConfig GetInstance()
+        {
+            if (_configs == null)
+                _configs = new GlobalConfig();
+            return _configs;
+        }
+    }
+}
