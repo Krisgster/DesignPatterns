926e60bfd29085f8e91db40ce6dee8ffce36c3cd
Krisgster <35160463+Krisgster@users.noreply.github.com>
2018-01-06 14:24:29 +0700

    Add files via upload

---
+++ ConfigPrototype.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Create_Prototype
+{
+    public abstract class ConfigPrototype
+    {
+        public int Id { get; set; }
+        public ConfigPrototype(int id)
+        {
+            Id = id;
+        }
+
+        public abstract ConfigPrototype Clone();
+    }
+}
---
+++ ConreateConfigPrototype.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Create_Prototype
+{
+    public class ConreateConfigPrototype : ConfigPrototype
+    {
+        public ConreateConfigPrototype(int id) : base(id)
+        {
+
+        }
+        public override ConfigPrototype Clone()
+        {
+            return (ConfigPrototype)this.MemberwiseClone();
+        }
+    }
+}
