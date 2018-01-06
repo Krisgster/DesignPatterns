12c98cb0d92d37bd301bf654cd0a1c54b8e4c753
Krisgster <35160463+Krisgster@users.noreply.github.com>
2018-01-06 14:23:44 +0700

    Add files via upload

---
+++ ConreteCreator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Patterns.Create_Factory;
+using Patterns.Helpers;
+
+namespace Patterns.Create_FactoryMethod
+{
+    public class ConreteCreator : Creator,IProduct
+    {
+        public IProduct CreateProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IntroduceYourSelf(string owner)
+        {
+            Console.WriteLine("Hello, I am concrete creator! I was created by {0}", owner);
+        }
+
+        protected override IProduct CreateInstance()
+        {
+            return new ConreteCreator();
+        }
+    }
+}
---
+++ Creator.cs
@@ -0,0 +1,19 @@
+﻿using Patterns.Create_Factory;
+using Patterns.Helpers;
+
+namespace Patterns.Create_FactoryMethod
+{
+    public abstract class Creator
+    {
+        /// <summary>
+        /// This is factory method to create instance, will be implement by sub class
+        /// </summary>
+        /// <returns></returns>
+        protected abstract IProduct CreateInstance();
+        public void IntroduceInstance()
+        {
+            IProduct instance = CreateInstance();
+            instance.IntroduceYourSelf("Factory Method");
+        }
+    }
+}
